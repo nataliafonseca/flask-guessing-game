b4cfac902c012279183cf26d8ced34c790b1bf13
Natália Fonseca <nataliabf@outlook.com>
2022-02-26 21:00:19 -0300

    chore: configure deploy

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request
+import os
 import random
+
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -26,4 +28,6 @@ def result():
 
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
     app.run()
